7759e23d006fee938c29427081e2dc4bc8830ff3
Sony Ah-Sam <sony.ahsam1@gmail.com>
2024-02-07 22:22:14 +0100

    refactor: remove redundant <script> tag and update javascript code

--- assets/script.js
+++ assets/script.js
@@ -1,31 +1,29 @@
 const slides = [
 	{
-		"image":"slide1.jpg",
-		"tagLine":"Impressions tous formats <span>en boutique et en ligne</span>"
+		"image": "slide1.jpg",
+		"tagLine": "Impressions tous formats <span>en boutique et en ligne</span>"
 	},
 	{
-		"image":"slide2.jpg",
-		"tagLine":"Tirages haute définition grand format <span>pour vos bureaux et events</span>"
+		"image": "slide2.jpg",
+		"tagLine": "Tirages haute définition grand format <span>pour vos bureaux et events</span>"
 	},
 	{
-		"image":"slide3.jpg",
-		"tagLine":"Grand choix de couleurs <span>de CMJN aux pantones</span>"
+		"image": "slide3.jpg",
+		"tagLine": "Grand choix de couleurs <span>de CMJN aux pantones</span>"
 	},
 	{
-		"image":"slide4.png",
-		"tagLine":"Autocollants <span>avec découpe laser sur mesure</span>"
+		"image": "slide4.png",
+		"tagLine": "Autocollants <span>avec découpe laser sur mesure</span>"
 	}
 ]
 
-document.addEventListener('DOMContentLoaded', function() {
-    const arrow_left = document.querySelector(".arrow_left");
-    const arrow_right = document.querySelector(".arrow_right");
+const arrow_left = document.querySelector(".arrow_left");
+const arrow_right = document.querySelector(".arrow_right");
 
-    arrow_left.addEventListener("click", () => {
-        console.log("flèche gauche cliquée");
-    });
+arrow_left.addEventListener("click", () => {
+	console.log("flèche gauche cliquée");
+});
 
-    arrow_right.addEventListener("click", () => {
-        console.log("flèche droite cliquée");
-    });
+arrow_right.addEventListener("click", () => {
+	console.log("flèche droite cliquée");
 });
